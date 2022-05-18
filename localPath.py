74e78291cf1ab91599c43893fe8c159539ea8f5f
DelGuardiano <74706390+DelGuardiano@users.noreply.github.com>
2022-05-18 13:05:14 +0200

    Ændret path så den passer til skole pc, samt tilføjet den streng som skal køres for at den starter

--- localPath.py
+++ localPath.py
@@ -1,10 +1,10 @@
-modelpath = 'C:/Users/surin/OneDrive/Dokumenter/GitHub/4-Semester-Projekt/model/model.h5'
+modelpath = 'D:/SimpsonsGenkendelse/model/model.h5'
 
-samplepath = 'C:/Users/surin/OneDrive/Dokumenter/GitHub/4-Semester-Projekt\Input/the-simpsons-dataset/'
+samplepath = 'D:/SimpsonsGenkendelse/Input/the-simpsons-dataset/'
 
-legendplot = 'C:/Users/surin/OneDrive/Dokumenter/GitHub/4-Semester-Projekt/model/diagrammer/legendplot.png'
+legendplot = 'D:/SimpsonsGenkendelse/model/diagrammer/legendplot.png'
 
-boxplot = 'C:/Users/surin/OneDrive/Dokumenter/GitHub/4-Semester-Projekt/model/diagrammer/boxplot.png'
+boxplot = 'D:/SimpsonsGenkendelse/model/diagrammer/boxplot.png'
 
 
 
--- predict.py
+++ predict.py
@@ -1,4 +1,4 @@
-from keras.preprocessing.image import img_to_array, load_img
+from keras_preprocessing.image import img_to_array, load_img
 from keras.models import load_model
 import numpy as np
 import pandas as pd
